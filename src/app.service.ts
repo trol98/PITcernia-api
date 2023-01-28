fc6b3f1ce31cf670382290901daf59fe2042ff14
Seweryn Szymon <seweryn.szymon@shiftme.pl>
2023-01-28 15:49:32 +0100

    Add configService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    // To be removed
+    // Just an example on how to use configService
+    return 'Hello World on: ' + this.configService.get('PORT');
   }
 }
